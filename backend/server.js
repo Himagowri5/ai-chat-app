250ec7a28a2928d0ecbb8bc4469bdc0f10d3dd9f
Himagowri Nadig <your-email@example.com>
2026-09-24 19:37:57 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,13 @@ import session from "express-session";
 import passport from "passport";
 import "./config/passport.js";
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors({
-        origin: "http://localhost:5173",
-        credentials: true
-    }
-));
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}));
 app.use(
     session({
         secret: process.env.SESSION_SECRET,
